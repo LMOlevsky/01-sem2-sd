cb7a967b1aeb131ef63aeaf9ce45c315d3bd5a8d
unknown <lolevsky@stuy.edu>
2018-02-08 22:05:23 -0500

    finished hw

--- canvas.js
+++ canvas.js
@@ -1,31 +1,88 @@
 var c = document.getElementById("slate");
 var ctx = c.getContext('2d');
 var requestID;
-var x = 0;
 
-var growCircle = function(){
-    ctx.clearRect(0,0,500,500);
-    ctx.beginPath();
-    ctx.arc(c.width/2, c.height/2, x, 0, 2*Math.PI);
-    ctx.lineWidth = 5;
-    ctx.stroke();
-    ctx.fillStyle = "red";
-    ctx.fill();
+var circleAnim = function(){
 
-    x=x+0.5;
-    requestID = window.requestAnimationFrame( growCircle );
+    stop();
+    
+    var x = 10;
+    var grow = true;
+    
+    var circle = function(){
+	ctx.clearRect(0,0,c.width,c.height);
+	ctx.beginPath();
+	ctx.arc(c.width/2, c.height/2, x, 0, 2*Math.PI);
+	ctx.fillStyle = "red";
+	ctx.fill();
+
+	if (grow){
+	    x++;
+	}
+	else{
+	    x--;  
+	}
+	if (x >= c.height/2){
+	    grow = false
+	}
+	if (x <= 0){
+	    grow = true;
+	}
+	
+	requestID = window.requestAnimationFrame( circle );
+    }
+
+    window.requestAnimationFrame( circle );
+
 }
 
-var stopGrowth = function(){
+var screensaverAnim = function(){
+
+    stop();
+    
+    var x = Math.random()*c.width;
+    var y = Math.random()*c.height;
+    var down = true;
+    var right = true;
+    
+    var screensaver = function(){
+	ctx.clearRect(0,0,c.width,c.height);
+	ctx.fillRect(x, y, 20, 20)
+	ctx.fillStyle = "red";
+	ctx.fill();
+
+
+	if (down){
+	    y+=2;
+	}
+	else{
+	    y-=2;
+	}
+	if(right){
+	    x+=2;
+	}
+	else{
+	    x-=2;
+	}
+
+	if(x >= c.width-20 || x <= 0){
+	    right = !right;
+	}
+	if(y >= c.height-20 || y <= 0){
+	    down = !down; 
+	}	
+	
+	requestID = window.requestAnimationFrame( screensaver );
+    }
+
+    window.requestAnimationFrame( screensaver );
+
+}
+
+var stop = function(){
     window.cancelAnimationFrame(requestID);
 }
 
-var clearScreen = function(){
-    ctx.clearRect(0,0,c.width,c.height)
-}
-
-//window.requestAnimationFrame( growCircle );
-
-stop.addEventListener('click', stopGrowth);
-clear.addEventListener('click', clearScreen);
-
+stopAnim.addEventListener('click', stop);
+screensaver.addEventListener('click', screensaverAnim);
+circle.addEventListener('click', circleAnim);
